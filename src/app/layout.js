bb6130818eb2fa0343348fe4b74e4a62850b114c
MetaCapFin <advisor@metacap.finance>
2025-02-24 14:54:43 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <link rel="icon" href="/Logo.png" type="image/png" />
